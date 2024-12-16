1c8ff1fd450e6cff699adfedb29bdf424661f034
Henry Santiago <hjsantiago13@gmail.com>
2024-12-16 16:03:25 +0900

    Added ruby challenges

---
+++ greater-than-5.rb
@@ -0,0 +1,18 @@
+=begin 1. Numbers greater than 5
+This problem involves finding the number of integers with a value greater than 5. In Ruby speak, you could put it like this: Given an array, count how many items are greater than 5.
+To solve this, you begin by outlining the logic, then translating it into code. An array is a collection of data.
+For example:
+Array = [17, 7, 3, 6, 10, 1]
+First, you go through the entire array and ask if each number is greater than 5.
+You then create a “count” variable by checking if each number has a value higher than 5. If it is, you count it, and if not, you don’t.
+=end
+
+numbers = [17, 7, 3, 6, 10, 1]
+count = 0
+numbers.each do |n|
+  if n > 5
+    count += 1
+  end
+end
+
+puts "count of numbers greater than 5 is #{count}"
